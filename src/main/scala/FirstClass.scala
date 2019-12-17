9324b28e54170a6f40f80b8257b2f2f4b0614600
uenz98 <45397398+uenz98@users.noreply.github.com>
2019-12-17 12:16:39 +0100

    modified FirstClass

--- src/main/scala/FirstClass.scala
+++ src/main/scala/FirstClass.scala
@@ -1,7 +1,21 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive.HiveContext
+
 object FirstClass{
   def main(args: Array[String]) {
-    println("ciao")
-    println("test commit/push 2")
+
+    val conf = new SparkConf().setMaster("local[2]")
+      .setAppName("CountingSheep")
+
+    val sc = new SparkContext(conf)
+
+    //parsing JSON
+    val sqlContext = new HiveContext(sc)
+
+    val jsonDF = sqlContext.read.json("C:\\Users\\Studente\\Desktop\\gesudinazaret\\2018-03-01-0.json.gz")
+    //println(jsonDF.toString)
+
+    jsonDF.show
 
   }
 }
