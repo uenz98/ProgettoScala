8cd1475d120462e16411f64e3b6af97730e48e9d
muusta45 <muusta45@gmail.com>
2019-12-17 17:23:01 +0100

    added ScalaTest (payload da sistemare)

---
+++ src/test/scala/PayloadTest.scala
@@ -0,0 +1,23 @@
+
+import entities.Payload
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{FlatSpec, Matchers}
+
+class PayloadTest extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("CountingSheep")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new HiveContext(sc)
+    val jsonDF = sqlContext.read.json("payloadTest.json")
+
+    import sqlContext.implicits._
+    val rdd = jsonDF.as[Payload].rdd
+
+    rdd.foreach(println)
+
+    assert(true)
+  }
+}
---
+++ src/test/scala/RepoTest.scala
@@ -0,0 +1,23 @@
+
+import entities.Repo
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{FlatSpec, Matchers}
+
+class RepoTest extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("CountingSheep")
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new HiveContext(sc)
+    val jsonDF = sqlContext.read.json("repoTest.json")
+
+    import sqlContext.implicits._
+    val rdd = jsonDF.as[Repo].rdd
+
+    rdd.foreach(println)
+
+    assert(true)
+  }
+}
